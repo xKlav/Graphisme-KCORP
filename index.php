2cc01585258906271d658e9de0bdf1abf16052d0
Mathis <mathis.donnez@gmail.com>
2022-05-11 03:12:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width , initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <title>Graphism</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="team">
+            <img class="bgkc" src="assets/img/team.png" alt="">
+            <div class="title">
+                <span>League</span>
+                <span>Of</span>
+                <span>Legends</span>
+                <span>E-Sport Team</span>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="cabochard">
+            <div class="leftcabo">
+                <img class="top" src="assets/img/Roles/top.png" alt="">
+                <h2>Lucas Simon-Meslet "Cabochard" (né le 15 avril 1997) est un joueur français en contrat avec la Karmine Corp en temps que toplaner.<br><br>
+                    Figure incontournable de la scène FR depuis de longues années, son arrivée chez la KCORP n'est pas passée innaperçue.<br><br>
+                    Nous pouvons le retrouver au top sur des personnages tels que LeeSin, Graves ou encore Irelia.</h2>
+            </div>
+            <div class="centercabo">
+                <h1>Cabochard</h1>
+                <img class="imgcabo" src="assets/img/equipe/cabochard.jpg_large" alt="">
+            </div>
+            <div class="rightcabo">
+                <img class="leesin" src="assets/img/personnages/leesin.png" alt="">
+                <img class="graves" src="assets/img/personnages/graves.png" alt="">
+                <img class="irelia" src="assets/img/personnages/irelia.png" alt="">
+
+            </div>
+        </div>
+
+        <div class="bumm">
+            <div class="leftbumm">
+                <img class="volibear" src="assets/img/personnages/volibear.png" alt="">
+                <img class="xin" src="assets/img/personnages/xin.png" alt="">
+                <img class="j4" src="assets/img/personnages/j4.png" alt="">
+            </div>
+            <div class="centerbumm">
+                <h1>113</h1>
+                <img class="imgbumm" src="assets/img/equipe/113.png" alt="">
+            </div>
+            <div class="rightbumm">
+                <img class="jgl" src="assets/img/Roles/jungle.png" alt="">
+                <h2>Doğukan Balcı "113" (né le 12 août 2004) est un joueur turque en contrat avec la Karmine Corp en temps que jungler.<br><br>
+                    113 ou "bumm" est passé challenger pour la première fois à 14 ans. Il a remporté la LVP Superliga avec Vodafone Giants à l'été 2021.<br><br>
+                    Nous pouvons le retrouver en jungle sur des personnages tels que Jarvan IV, Volibear ou encore Xin Zhao.</h2>
+            </div>
+        </div>
+
+        <div class="saken">
+            <div class="leftsaken">
+            <img class="mid" src="assets/img/Roles/mid.png" alt="">
+                <h2>Lucas Fayard "Saken" (né le 5 novembre 1998) est un joueur français en contrat avec la Karmine Corp en temps que midlaner.<br><br>
+                    Il rejoint la structure à sa création pour la saison de 2021 en LFL. En ce début de saison de 2022, c’est le seul membre restant de sa formation initiale.<br><br>
+                    Nous pouvons le retrouver au mid sur des personnages tels que Ryze, Leblanc ou encore Azir.</h2>
+            </div>
+            <div class="centersaken">
+                <h1>Saken</h1>
+                <img class="imgsaken" src="assets/img/equipe/saken.jfif" alt="">
+            </div>
+            <div class="rightsaken">
+                <img class="ryze" src="assets/img/personnages/ryze.png" alt="">
+                <img class="azir" src="assets/img/personnages/azir.png" alt="">
+                <img class="leblanc" src="assets/img/personnages/leblanc.png" alt="">
+            </div>
+        </div>
+
+        <div class="rekkles">
+            <div class="leftrekkles">
+                <img class="jhin" src="assets/img/personnages/jhin.png" alt="">
+                <img class="jinx" src="assets/img/personnages/jinx.png" alt="">
+                <img class="tristana" src="assets/img/personnages/tristanapng.png" alt="">
+                
+            </div>
+            <div class="centerrekkles">
+                <h1>Rekkles</h1>
+                <img class="imgrekkles" src="assets/img/equipe/rekkles.avif" alt="">
+            </div>
+            <div class="rightrekkles">
+                <img class="adc" src="assets/img/Roles/adc.png" alt="">
+                <h2>Martin Larsson "Rekkles" (né le 20 septembre 1996) est un joueur suedois en contrat avec la Karmine Corp en temps que AD Carry.<br><br>
+                    Un des grands noms de l'esport sur League of Legends, quatre fois champion d'Europe avant son arrivée chez la KCORP. Sa venue dans l'équipe a enflammé la fanbase LoL française, pas étonannt vu qu'il est aujourd'hui consideré comme un des meilleurs ADC au monde.<br><br>
+                    Nous pouvons le retrouver en botlane sur des personnages tels que Jinx, Jhin ou encore Tristana.</h2>
+            </div>
+        </div>
+
+        <div class="hantera">
+            <div class="lefthantera">
+            <img class="supp" src="assets/img/Roles/supp.png" alt="">
+                <h2>Jules Bourgeois "Hantera"(né le 12 février 1999) est un joueur français en contrat avec la Karmine Corp en temps que support.<br><br>
+                    Ancien support de l'équipe Solary, il rejoint la KCORP contre toute attente afin de conquérir la botlane au côté de Rekkles. Durant l'été 2021, il a fait une deuxième place en LVP Superliga.<br><br>
+                    Nous pouvons le retrouver au mid sur des personnages tels que Pyke, Galio ou encore Thresh.</h2>
+            </div>
+            <div class="centerhantera">
+                <h1>Hantera</h1>
+                <img class="imghantera" src="assets/img/equipe/hantera.avif" alt="">
+            </div>
+            <div class="righthantera">
+                <img class="galio" src="assets/img/personnages/galio.png" alt="">
+                <img class="pyke" src="assets/img/personnages/pyke.webp" alt="">
+                <img class="thresh" src="assets/img/personnages/thresh.png" alt="">
+                
+            </div>
+        </div>
+
+    </main>
+    <footer></footer>
+</body>
+
+</html>
